66ceb4d22dd06811a310550b1248483597c9cc7b
Paulo Freitas <paulofreitasdn@outlook.com.br>
2024-05-29 08:15:12 -0300

    feat: initial config

---
+++ src/main/java/com/paulofreitas/auto_park/AutoParkApplication.java
@@ -0,0 +1,12 @@
+package com.paulofreitas.auto_park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoParkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/paulofreitas/auto_park/AutoParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulofreitas.auto_park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
