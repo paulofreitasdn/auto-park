c46b94f8b0f7bf7a3e7ac4654db2f27cff007bfb
Paulo Freitas <paulofreitasdn@outlook.com.br>
2024-06-01 11:08:58 -0300

    feat: initial commit

---
+++ src/main/java/com/paulofreitas/auto_park/config/SpringTimezoneConfig.java
@@ -0,0 +1,15 @@
+package com.paulofreitas.auto_park.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimezoneConfig {
+
+    @PostConstruct
+    public void timezoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+}
