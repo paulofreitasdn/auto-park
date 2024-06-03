0d1c75670c411b396c940a6dc63624ca9e1c8f00
Paulo Freitas <paulofreitasdn@outlook.com.br>
2024-06-03 08:55:48 -0300

    feat: initial commit

---
+++ src/main/java/com/paulofreitas/auto_park/entity/User.java
@@ -0,0 +1,67 @@
+package com.paulofreitas.auto_park.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "profile", nullable = false, length = 25)
+    private Profile profile;
+
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @Column(name = "modification_date")
+    private LocalDateTime modificationDate;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    public enum Profile {
+        PROFILE_ADMIN, PROFILE_CLIENT
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + '}';
+    }
+}
