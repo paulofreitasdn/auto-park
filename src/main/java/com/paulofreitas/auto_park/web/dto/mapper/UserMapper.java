ea739ff5ed63b955d7c25b00e046b2677f58179f
Paulo Freitas <paulofreitasdn@outlook.com.br>
2024-06-07 06:50:03 -0300

    feat: adding DTO in resource to list all users

--- src/main/java/com/paulofreitas/auto_park/web/dto/mapper/UserMapper.java
+++ src/main/java/com/paulofreitas/auto_park/web/dto/mapper/UserMapper.java
@@ -6,6 +6,9 @@ import com.paulofreitas.auto_park.web.dto.UserResponseDto;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserMapper {
 
     public static User toUser(UserCreateDto createDto) {
@@ -23,5 +26,9 @@ public class UserMapper {
         ModelMapper mapper = new ModelMapper();
         mapper.addMappings(props);
         return mapper.map(user, UserResponseDto.class);
+    }
+
+    public static List<UserResponseDto> toListDto(List<User> users) {
+        return users.stream().map(user -> toDto(user)).collect(Collectors.toList());
     }
 }
