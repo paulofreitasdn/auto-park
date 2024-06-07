1b0f669d5e5d8799c399af8e2786d164ad2b33ef
Paulo Freitas <paulofreitasdn@outlook.com.br>
2024-06-07 07:00:25 -0300

    feat: adjustments to the updatePassword method and corrections to the return type of the getAll method

--- src/main/java/com/paulofreitas/auto_park/web/controller/UserController.java
+++ src/main/java/com/paulofreitas/auto_park/web/controller/UserController.java
@@ -3,8 +3,10 @@ package com.paulofreitas.auto_park.web.controller;
 import com.paulofreitas.auto_park.entity.User;
 import com.paulofreitas.auto_park.service.UserService;
 import com.paulofreitas.auto_park.web.dto.UserCreateDto;
+import com.paulofreitas.auto_park.web.dto.UserPasswordDto;
 import com.paulofreitas.auto_park.web.dto.UserResponseDto;
 import com.paulofreitas.auto_park.web.dto.mapper.UserMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +22,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserResponseDto> create(@RequestBody UserCreateDto createDto) {
+    public ResponseEntity<UserResponseDto> create(@Valid @RequestBody UserCreateDto createDto) {
         User newUser = userService.toSave(UserMapper.toUser(createDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toDto(newUser));
     }
@@ -32,14 +34,14 @@ public class UserController {
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<User> updatePassword(@PathVariable Long id, @RequestBody User user) {
-        User updateUserPassword = userService.editPassword(id, user.getPassword());
-        return ResponseEntity.ok(updateUserPassword);
+    public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody UserPasswordDto dto) {
+        User user = userService.editPassword(id, dto.getCurrentPassword(), dto.getNewPassword(), dto.getConfirmPassword());
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping
-    public ResponseEntity<List<User>> getAll() {
+    public ResponseEntity<List<UserResponseDto>> getAll() {
         List<User> users = userService.searchAll();
-        return ResponseEntity.ok(users);
+        return ResponseEntity.ok(UserMapper.toListDto(users));
     }
 }
