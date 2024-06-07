d615b5e0f6de49a2bfda1ddc55ff7adb97c05e1c
Paulo Freitas <paulofreitasdn@outlook.com.br>
2024-06-07 06:42:51 -0300

    feat: annotations to validate attributes

--- src/main/java/com/paulofreitas/auto_park/web/dto/UserCreateDto.java
+++ src/main/java/com/paulofreitas/auto_park/web/dto/UserCreateDto.java
@@ -1,5 +1,8 @@
 package com.paulofreitas.auto_park.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,6 +12,11 @@ import lombok.*;
 @ToString
 public class UserCreateDto {
 
+    @NotBlank
+    @Email(message = "Email format is invalid", regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
     private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String password;
 }
