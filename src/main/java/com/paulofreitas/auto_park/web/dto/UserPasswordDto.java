26fe78f5b4a863d7da30123aa1de101f07e7ca51
Paulo Freitas <paulofreitasdn@outlook.com.br>
2024-06-12 08:48:54 -0300

    feat: Add data validations to DTO UserPasswordDto

--- src/main/java/com/paulofreitas/auto_park/web/dto/UserPasswordDto.java
+++ src/main/java/com/paulofreitas/auto_park/web/dto/UserPasswordDto.java
@@ -1,5 +1,7 @@
 package com.paulofreitas.auto_park.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,7 +11,15 @@ import lombok.*;
 @ToString
 public class UserPasswordDto {
 
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String currentPassword;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String newPassword;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String confirmPassword;
 }
